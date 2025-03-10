2cdbd54da547b9659693f29dafaa393887540b78
grandj161u <matheo.grandjean8@etu.univ-lorraine.fr>
2025-03-10 17:42:05 +0100

    exercices singleton fini

--- singleton/app/Config.php
+++ singleton/app/Config.php
@@ -3,3 +3,23 @@
 # TODO: Créer une classe Config en Singleton
 
 namespace App;
+
+class Config
+{
+
+    private static $instance;
+    private $config;
+
+    private function __construct()
+    {
+        $this->config = require(__DIR__ . '/../config/config.php');
+    }
+
+    public static function getInstance()
+    {
+        if (is_null(self::$instance)) {
+            self::$instance = new Config();
+        }
+        return self::$instance;
+    }
+}
--- singleton/public/index.php
+++ singleton/public/index.php
@@ -5,3 +5,10 @@ require('../vendor/autoload.php');
 # TODO: Récuperer une instance de Config
 # Afficher une valeur contenu dans config.php
 # Récupérer une seconde instance de Config et vérifié que les deux instances sont identiques
+
+$config = App\Config::getInstance();
+var_dump($config);
+$config2 = App\Config::getInstance();
+var_dump($config2);
+
+var_dump($config === $config2); // true
